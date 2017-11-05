38bc35697bc5e7bf1afebbba7a23295bbe437f6f
fischjer4 <fischjer4@gmail.com>
2017-11-05 12:08:47 -0800

    added crypto handle init and printk statements to __init function

--- Encrypted-Block-Device/block_dev_crypto.c
+++ Encrypted-Block-Device/block_dev_crypto.c
@@ -1,7 +1,8 @@
 /*
 	* The skeleton for this code was taken from Pat Paterson at
 		http://blog.superpat.com/2010/05/04/a-simple-block-driver-for-linux-kernel-2-6-31/
-
+	* Crypto resource
+		https://01.org/linuxgraphics/gfx-docs/drm/crypto/intro.html
 */
 
 
@@ -37,20 +38,14 @@ module_param(logical_block_size, int, 0);
 static int nsectors = 1024; /* How big the drive is */
 module_param(nsectors, int, 0);
 
-/*
- * We can tweak our hardware sector size, but the kernel talks to us
- * in terms of small sectors, always.
- */
+/*We can tweak our hardware sector size, but the kernel talks to us
+	in terms of 512 byte sectors */
 #define KERNEL_SECTOR_SIZE 512
 
-/*
- * Our request queue.
- */
+/*Request Queue*/
 static struct request_queue *Queue;
 
-/*
- * The internal representation of our device.
- */
+/*Internal Representation Of The Block Device*/
 static struct sbd_device {
 	unsigned long size;
 	spinlock_t lock;
@@ -58,8 +53,17 @@ static struct sbd_device {
 	struct gendisk *gd;
 } Device;
 
+/*Crypto Handle*/
+struct crypto_cipher *tfm;
+
+
+
+
+
+
+
 /*
- * Handle an I/O request.
+ 	* Handle an I/O request.
  */
 static void sbd_transfer(struct sbd_device *dev, sector_t sector,
 		unsigned long nsect, char *buffer, int write) {
@@ -98,9 +102,9 @@ static void sbd_request(struct request_queue *q) {
 }
 
 /*
- * The HDIO_GETGEO ioctl is handled in blkdev_ioctl(), which
- * calls this. We need to implement getgeo, since we can't
- * use tools such as fdisk to partition the drive otherwise.
+	* The HDIO_GETGEO ioctl is handled in blkdev_ioctl(), which
+	* calls this. We need to implement getgeo, since we can't
+	* use tools such as fdisk to partition the drive otherwise.
  */
 int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
 	long size;
@@ -115,7 +119,7 @@ int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
 }
 
 /*
- * The device operations structure.
+ 	* The device operations structure.
  */
 static struct block_device_operations sbd_ops = {
 		.owner  = THIS_MODULE,
@@ -123,35 +127,43 @@ static struct block_device_operations sbd_ops = {
 };
 
 static int __init sbd_init(void) {
-	/*
-	 * Set up our internal device.
-	 */
+	/*Set Up Kernel Internal Device*/
 	Device.size = nsectors * logical_block_size;
 	spin_lock_init(&Device.lock);
 	Device.data = vmalloc(Device.size);
 	if (Device.data == NULL)
 		return -ENOMEM;
-	/*
-	 * Get a request queue.
-	 */
+	
+	/*Initilize Crypto Handle*/
+	/*Middle arguement being 0 means use default cipher handling*/
+	tfm = crypto_alloc_cipher("aes", 0, 0);
+	if (IS_ERR(tfm)){
+		printk("~BLKDEVCRYPT~ sbd_init() -- [tfm] failed to initialize\n");
+		goto out;		
+	}
+	
+	/*Initilize The Request Queue */
 	Queue = blk_init_queue(sbd_request, &Device.lock);
-	if (Queue == NULL)
-		goto out;
-	blk_queue_logical_block_size(Queue, logical_block_size);
-	/*
-	 * Get registered.
-	 */
-	major_num = register_blkdev(major_num, "sbd");
-	if (major_num < 0) {
-		printk(KERN_WARNING "sbd: unable to get major number\n");
+	if (Queue == NULL){
+		printk("~BLKDEVCRYPT~ sbd_init() -- [Queue] failed to initialize\n");		
 		goto out;
 	}
-	/*
-	 * And the gendisk structure.
-	 */
+	blk_queue_logical_block_size(Queue, logical_block_size);
+
+	/*Register The Block Device*/
+	major_num = register_blkdev(major_num, "sbd");
+	if (major_num < 0) {
+		printk(KERN_WARNING "~BLKDEVCRYPT~ sbd_init() -- [major_num] failed to initialize\n");
+		goto out;
+	}
+	
+	/*And the gendisk structure*/
 	Device.gd = alloc_disk(16);
-	if (!Device.gd)
+	if (!Device.gd){
+		printk("~BLKDEVCRYPT~ sbd_init() -- [Device.gd] failed to initialize\n");				
 		goto out_unregister;
+	}
+
 	Device.gd->major = major_num;
 	Device.gd->first_minor = 0;
 	Device.gd->fops = &sbd_ops;
@@ -159,7 +171,10 @@ static int __init sbd_init(void) {
 	strcpy(Device.gd->disk_name, "sbd0");
 	set_capacity(Device.gd, nsectors);
 	Device.gd->queue = Queue;
+
+	/*Final Registration Step With Kernel*/
 	add_disk(Device.gd);
+	printk("~BLKDEVCRYPT~ sbd_init() -- Successfully initialized block device\n");				
 
 	return 0;
 
@@ -167,6 +182,8 @@ out_unregister:
 	unregister_blkdev(major_num, "sbd");
 out:
 	vfree(Device.data);
+	/*clear the crypto handle*/
+	crypto_free_cipher(tfm);
 	return -ENOMEM;
 }
 
@@ -177,6 +194,8 @@ static void __exit sbd_exit(void)
 	unregister_blkdev(major_num, "sbd");
 	blk_cleanup_queue(Queue);
 	vfree(Device.data);
+	/*clear the crypto handle*/
+	crypto_free_cipher(tfm);
 }
 
 MODULE_AUTHOR("Jeremy Fischer and Omeed");
