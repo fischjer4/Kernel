c67cbdce43cd7aebb5cfa1b9a684ef64bf5c844a
fischjer4 <fischjer4@gmail.com>
2017-11-05 14:40:31 -0800

    added a print_mem function which prints the elements of the buffer and memory of the block device

--- Encrypted-Block-Device/block_dev_crypto.c
+++ Encrypted-Block-Device/block_dev_crypto.c
@@ -1,19 +1,9 @@
-/*
-	* The skeleton for this code was taken from Pat Paterson at
-		http://blog.superpat.com/2010/05/04/a-simple-block-driver-for-linux-kernel-2-6-31/
-	* Crypto resource
-		https://01.org/linuxgraphics/gfx-docs/drm/crypto/intro.html
-*/
-
-
-
-/*
- * A sample, extra-simple block driver. Updated for kernel 2.6.31.
- *
- * (C) 2003 Eklektix, Inc.
- * (C) 2010 Pat Patterson <pat at superpat dot com>
- * Redistributable under the terms of the GNU GPL.
- */
+ /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*	- The skeleton for the block device code was taken from Pat Paterson at
+*		http://blog.superpat.com/2010/05/04/a-simple-block-driver-for-linux-kernel-2-6-31/
+*	- Crypto resource used
+*		https://01.org/linuxgraphics/gfx-docs/drm/crypto/intro.html
+  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -39,14 +29,14 @@ module_param(logical_block_size, int, 0);
 static int nsectors = 1024; /* How big the drive is */
 module_param(nsectors, int, 0);
 
-/*We can tweak our hardware sector size, but the kernel talks to us
+/* We can tweak our hardware sector size, but the kernel talks to us
 	in terms of 512 byte sectors */
 #define KERNEL_SECTOR_SIZE 512
 
-/*Request Queue*/
+/* Request Queue */
 static struct request_queue *Queue;
 
-/*Internal Representation Of The Block Device*/
+/* Internal Representation Of The Block Device */
 static struct sbd_device {
 	unsigned long size;
 	spinlock_t lock;
@@ -54,15 +44,31 @@ static struct sbd_device {
 	struct gendisk *gd;
 } Device;
 
-/*Crypto Handle*/
+/* The device operations structure */
+static struct block_device_operations sbd_ops = {
+		.owner  = THIS_MODULE,
+		.getgeo = sbd_getgeo
+};
+
+/* Crypto Handle */
 struct crypto_cipher *tfm;
 
 
 
 
 
-
-
+/*
+	* Print limit bytes from source
+	* Used to print from the buffer and block device memory
+*/
+static void print_mem(source, limit){
+	unsigned int i = 0;
+	while (i < limit){
+		printk("%c", source[i]);
+		i++;
+	}
+	printk("\n");
+}
 /*
  	* Handle an I/O request.
  */
@@ -85,27 +91,35 @@ static void sbd_transfer(struct sbd_device *dev, sector_t sector,
 	*/
 	printk("Read/Write %lu bytes from the block device", nbytes);
 	if (write){
-		printk("~BLKDEVCRYPT~ sbd_transfer() -- Write: before encryption: %s\n", buffer);			
+		printk("~BLKDEVCRYPT~ sbd_transfer() -- Write: before encryption: ");	
+		print_mem(buffer, nbytes);			
 		while(i < nbytes){
 			crypto_cipher_encrypt_one(tfm, (dev->data + offset + i), buffer + i);
 			i += crypto_cipher_blocksize(tfm) //go to next block
 		}
-		printk("~BLKDEVCRYPT~ sbd_transfer() -- Write: after encryption: %s\n", dev->data + offset);					
+		printk("~BLKDEVCRYPT~ sbd_transfer() -- Write: after encryption: ");					
+		print_mem(dev->data + offset, nbytes);					
 	}
 	else{
-		printk("~BLKDEVCRYPT~ sbd_transfer() -- Read: before decryption: %s\n", dev->data + offset);							
+		printk("~BLKDEVCRYPT~ sbd_transfer() -- Read: before decryption: ");					
+		print_mem(dev->data + offset, nbytes);								
 		while(i < nbytes){
 			crypto_cipher_decrypt_one(tfm, buffer + i, (dev->data + offset + i));
 			i += crypto_cipher_blocksize(tfm) //go to next block
 		}
-		printk("~BLKDEVCRYPT~ sbd_transfer() -- Read: after decryption: %s\n", buffer);					
+		printk("~BLKDEVCRYPT~ sbd_transfer() -- Read: after decryption: ");					
+		print_mem(buffer, nbytes);					
 	}
 }
-
+/*
+	* Get the next request in the Queue
+	* If a request isn't finished, keep working on it
+		If it is, get the next one in line
+*/
 static void sbd_request(struct request_queue *q) {
 	struct request *req;
 
-	/*get next request*/
+	/* get next request */
 	req = blk_fetch_request(q);
 	while (req != NULL) {
 		if (req == NULL || (req->cmd_type != REQ_TYPE_FS)) {
@@ -114,13 +128,12 @@ static void sbd_request(struct request_queue *q) {
 			continue;
 		}
 		sbd_transfer(&Device, blk_rq_pos(req), blk_rq_cur_sectors(req), req->buffer, rq_data_dir(req));
-		/*if this req is done, get the next one*/
+		/* if this req is done, get the next one */
 		if ( ! __blk_end_request_cur(req, 0) ) {
 			req = blk_fetch_request(q);
 		}
 	}
 }
-
 /*
 	* The HDIO_GETGEO ioctl is handled in blkdev_ioctl(), which
 	* calls this. We need to implement getgeo, since we can't
@@ -137,15 +150,14 @@ int sbd_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
 	geo->start = 0;
 	return 0;
 }
-
 /*
- 	* The device operations structure.
- */
-static struct block_device_operations sbd_ops = {
-		.owner  = THIS_MODULE,
-		.getgeo = sbd_getgeo
-};
-
+	* Initialize Block Device With Kernel, which includes:
+		- block size
+		- major number
+		- crypto handle
+		- request queue
+		- gendisk structure
+*/
 static int __init sbd_init(void) {
 	/*Set Up Kernel Internal Device*/
 	Device.size = nsectors * logical_block_size;
@@ -206,7 +218,9 @@ out:
 	crypto_free_cipher(tfm);
 	return -ENOMEM;
 }
-
+/*
+	* Exit the module, but first clean up
+*/
 static void __exit sbd_exit(void)
 {
 	del_gendisk(Device.gd);
