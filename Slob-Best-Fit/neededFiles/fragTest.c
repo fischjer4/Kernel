506d95a5023178d28422cc733af8ddb466f287f2
fischjer4 <fischjer4@gmail.com>
2017-11-28 15:33:50 -0800

    Realized to test slob, kernel space methods need to be used. So, instead there will be 5 iterations where each one makes the system calls

--- Slob-Best-Fit/neededFiles/fragTest.c
+++ Slob-Best-Fit/neededFiles/fragTest.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
+// #include <linux/kernel.h>
+// #include <sys/syscall.h>
 #include <unistd.h>
 
 int main(){  
 	unsigned long amt_claimed, amt_free;
-	char *dummy1, dummy2, dummy3, dummy4,dummy5,dummy6, dummy7;
+	int i;
 
-	printf("------------------------------------------\n");
-	printf("|  Allocating 7 buffers of heap memory   \n");
-	print("|										 \n");
-	printf("|\n");
-	dummy1 = (char*)malloc(100);
-	dummy2 = (char*)malloc(7553);
-	dummy3 = (char*)malloc(1456);
-	dummy4 = (char*)malloc(894);
-	dummy5 = (char*)malloc(1555);
-	dummy6 = (char*)malloc(5);
-	dummy7 = (char*)malloc(15729);
+	for(i = 0; i < 5; i++){
+		amt_claimed = syscall(360);
+		amt_free= syscall(359);
 
-	amt_claimed = syscall(360);
-	amt_free= syscall(359);
-
-	printf("|------------------------------------------");
-	printf("| Amount  Claimed: %lu\n", amt_claimed);
-	printf("| Amount  Free: %lu\n", amt_free);
-	printf("| Pefcent Claimed: %lu\n", 100 * (amt_claimed / amt_free) );
-	printf("------------------------------------------\n\n");
-
+		printf("|------------------------------------------");
+		printf("| Amount  Claimed: %lu\n", amt_claimed);
+		printf("| Amount  Free: %lu\n", amt_free);
+		printf("| Pefcent Claimed: %lu\n", 100 * (amt_claimed / amt_free) );
+		printf("------------------------------------------\n\n");
+		sleep(5);
+	}
      return 0;
 }
