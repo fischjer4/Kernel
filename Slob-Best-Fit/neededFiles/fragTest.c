f363858f1eef69e8d66cdaf4cc53621dd70e89f2
fischjer4 <fischjer4@gmail.com>
2017-12-01 18:06:57 -0800

    change fragTest

--- Slob-Best-Fit/neededFiles/fragTest.c
+++ Slob-Best-Fit/neededFiles/fragTest.c
@@ -8,12 +8,12 @@ int main(){
 	for(i = 0; i < 3; i++){
 		amt_claimed = syscall(360);
 		amt_free= syscall(359);
-		long double percent = (100 * ((long double)amt_free / (long double)amt_claimed));
+		long double effeciency = (100 * ((long double)amt_free / (long double)amt_claimed));
 
 		printf("|------------------------------------------\n");
 		printf("| Amount  Claimed: %lu\n", amt_claimed);
 		printf("| Amount  Free: %lu\n", amt_free);
-		printf("| Pefcent Claimed: %Lf\n", percent);
+		printf("| Free space / Claimed space: %Lf\n", effeciency);
 		printf("|------------------------------------------\n\n");
 		sleep(2);
 	}
